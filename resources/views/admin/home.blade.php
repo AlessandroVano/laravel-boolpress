2e19f72bd7d84977f585977998141e6dbd8d5944
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 15:52:48 +0100

    print the data of the users logged in the admin folder

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,18 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    {{-- Auth::user (ISTANZA)  ->NOME DELLA COLONNA CHE VOGLIAMO STAMPARE --}}
+                    <h1>Benvenuto {{ Auth::user()->name }} alla Dashboard Admin</h1>
+                    <h4>Il tuo User ID è: {{ Auth::id() }}</h4>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                   <h2>Completa il tuo profilo qui sotto</h2>
 
-                    {{ __('You are logged in!') }}
+                   <ul>
+                       <li>
+                           <a href="">Lorem ipsum dolor</a>
+                       </li>
+                       <li>
+                        <a href="">Lorem ipsum dolor</a>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum dolor</a>
+                    </li>
+                   </ul>
+
+            
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/* use illuminate\Support\Facades\Auth; */
 
 /*
 |--------------------------------------------------------------------------
