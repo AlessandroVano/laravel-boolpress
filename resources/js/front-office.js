7978512130907c7e96ffe8619438f9936e255692
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 17:06:21 +0100

    import vue and istance vue in file front-office.js

--- resources/js/front-office.js
+++ resources/js/front-office.js
@@ -1,1 +1,16 @@
-console.log('FRONT OFFICE');
+/* console.log('FRONT OFFICE'); */
+
+/* IMPORTAZIONE DI VUE */
+import Vue from 'vue';
+
+/* IMPORTARE APP.VUE */
+
+import App from './views/App.vue';
+
+
+// QUI METTIAMO L'ISTANZA DI VUE
+
+const root = new Vue({
+    el: '#root',
+    render: h => h(App) // Mostriamo app all'avvio di Vue
+})
---
+++ resources/js/views/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>Questo sito è in costruzione, ritorna tra qualche giorno per vedere contenuti</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss">
+
+   div {
+       h1 {
+           margin-top: 80px;
+            text-align: center;
+       }
+   }
+
+</style>
