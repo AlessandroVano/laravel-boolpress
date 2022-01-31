dd46dac484655f4eb0cff814feb20cd962ca4443
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 17:30:01 +0100

    push date seeder in db + modify databaseSeeder to run them in cascade

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        /* si decomenta e si fa questo per non scrivere ogni volta -classe per ogni seeder che devo far migrare nel DB (li facciamo andare a cascata)*/
+         $this->call([
+             PostsTableSeeder::class, /* facendo così mi basterà lanciare solo il comando php artisan db:seed (senza utilizzare la fleg di classe) */
+         ]);
     }
 }
