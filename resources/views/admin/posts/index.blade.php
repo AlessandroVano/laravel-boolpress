1ef6d552fda01a7646940bbb612b532a27fe1fea
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 18:29:00 +0100

    insert table in index.blade

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,9 +6,27 @@
 
      @if ($posts->isEmpty()) {{-- questo ritorna un true o false quando non trova posts ci fa capire che effettivamente non ci sono --}}
      <p>Nessun post trovato. <a href="{{route('admin.posts.create')}}"> Crea un post ora!!</a></p>
-         
      @else
-         
+         <table class="table">
+             <thead>
+                 <tr>
+                     <th>ID</th>
+                     <th>Title</th>
+                     <th colspan="3">Actions</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 @foreach ($posts as $post)
+                     <tr>
+                         <td> {{$post->id}} </td>
+                         <td> {{$post->title}} </td>
+                         <td>SHOW</td>
+                         <td>EDIT</td>
+                         <td>DELETE</td>
+                     </tr>
+                 @endforeach
+             </tbody>
+         </table>
      @endif
  </div>
 @endsection
