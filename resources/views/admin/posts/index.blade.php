ea6adf2da707d1ebe2ec83f1e41076792cf41fbb
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-31 18:24:06 +0100

    add if ->isEmpty

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,5 +3,12 @@
 @section('content')
  <div class="container">
      <h1>Blog</h1>
+
+     @if ($posts->isEmpty()) {{-- questo ritorna un true o false quando non trova posts ci fa capire che effettivamente non ci sono --}}
+     <p>Nessun post trovato. <a href="{{route('admin.posts.create')}}"> Crea un post ora!!</a></p>
+         
+     @else
+         
+     @endif
  </div>
 @endsection
