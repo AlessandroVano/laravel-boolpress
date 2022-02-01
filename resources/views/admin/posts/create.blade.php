afc57081ad0029edc1b1090fdf93ec821b91351b
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-02-01 14:52:33 +0100

    insert message errore in folder create.blade.php

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,16 +4,33 @@
     <div class="container">
         <h1 class="mb-5">Create New Post</h1>
 
+
+       @if ($errors->any())
+       <div class="alert alert-danger">
+           <ul>
+               @foreach ($errors->all() as $error )
+                   <li> {{$error}} </li>
+               @endforeach
+           </ul>
+       </div>
+       @endif
+
         <form action="{{ route('admin.posts.store') }}" method="POST">
             @csrf
 
             <div class="mb-3">
                 <label for="title" class="form-label">Title*</label>
-                <input class="form-control" type="text" name="title" id="title">
+                <input class="form-control" type="text" name="title" id="title" value="{{ old('title') }}">
+                @error('title')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="body" class="form-label">Body*</label>
-                <textarea class="form-control" name="body" id="body" rows="5"></textarea>
+                <textarea class="form-control" name="body" id="body" rows="5">{{ old('body') }}</textarea>
+                @error('body')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <button class="btn btn-primary" type="submit">Create Post</button>
